e61ec25293e9aa14724f76f5619062494fc2eac4
Благушин Ростислав Фёдорович <Blagushin.R.F@sberbank.ru>
2021-03-16 18:13:07 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {CKEditor} from '@ckeditor/ckeditor5-react';
+import BalloonEditor from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor";
+import '@ckeditor/ckeditor5-build-classic/build/translations/ru'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Using CKEditor 5 build in React</h2>
+                <CKEditor
+                    editor={BalloonEditor}
+                    data="<p>Type Your Text</p>"
+                    onReady={() => {
+                        // You can store the "editor" and use when it is needed.
+                    }}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        console.log( {event, editor,  data});
+                    }}
+                    config={{
+                        toolbar: {
+                            items: [
+                                'heading',
+                                '|',
+                                'bold',
+                                'italic',
+                                'link',
+                                'bulletedList',
+                                'numberedList',
+                                '|',
+                                '|',
+                                'imageUpload',
+                                'blockQuote',
+                                'insertTable',
+                                'mediaEmbed',
+                                'undo',
+                                'redo',
+                                'alignment',
+                                'fontSize',
+                                'highlight'
+                            ]
+                        },
+                        language: 'ru',
+                        image: {
+                            toolbar: [
+                                'imageTextAlternative',
+                                'imageStyle:full',
+                                'imageStyle:side'
+                            ]
+                        },
+                        table: {
+                            contentToolbar: [
+                                'tableColumn',
+                                'tableRow',
+                                'mergeTableCells'
+                            ]
+                        }
+                    }}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
